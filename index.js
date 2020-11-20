016b974db81f21ae17d72aba21802cf2112984bd
joelsonolissis <olissismorais1@outlook.com>
2020-11-20 08:13:18 +0000

    userRoute adicionado

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res)=> res.send('Ola mundo'));
-app.listen(port, () => console.log('Rodando porata 3000'));
+const express = require('express')
+const app = express()
+const port = 3000
+const userRoute = require('./node-express/routes/userRoute')
+userRoute(app)
+app.get('/', (req, res)=> res.send('Ola mundo'))
+app.listen(port, () => console.log('Rodando porata 3000'))
